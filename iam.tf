9f5b2136c27c7d1d372183853330019023741f10
spenes <enes@snowplowanalytics.com>
2022-01-08 02:55:58 +0300

    Initial release

---
+++ iam.tf
@@ -0,0 +1,62 @@
+data "aws_caller_identity" "current" {}
+
+data "aws_iam_policy_document" "snowflake_load_policy" {
+  statement {
+    actions = [
+      "s3:GetObject",
+      "s3:GetObjectVersion",
+    ]
+    resources = ["arn:aws:s3:::${var.stage_bucket_name}/${var.stage_prefix}/*"]
+  }
+
+  statement {
+    actions = [
+      "s3:ListBucket",
+      "s3:GetBucketLocation",
+    ]
+    condition {
+      test     = "StringLike"
+      variable = "s3:prefix"
+      values = ["${var.stage_prefix}/*"]
+    }
+    resources = ["arn:aws:s3:::${var.stage_bucket_name}"]
+  }
+}
+
+data "aws_iam_policy_document" "snowflake_load_assume_role_policy_storage_integration" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [snowflake_storage_integration.integration.storage_aws_iam_user_arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "sts:ExternalId"
+
+      values = [snowflake_storage_integration.integration.storage_aws_external_id]
+    }
+  }
+}
+
+resource "aws_iam_policy" "snowflakedb_load_policy" {
+  name        = local.snowflake_load_role_name
+  description = "Access policy for the SnowflakeLoadRole"
+  policy      = data.aws_iam_policy_document.snowflake_load_policy.json
+}
+
+resource "aws_iam_role" "snowflakedb_load_role" {
+  name                 = local.snowflake_load_role_name
+  description          = "Role for the Snowplow Snowflake Loader to assume"
+  max_session_duration = 43200
+  assume_role_policy   = data.aws_iam_policy_document.snowflake_load_assume_role_policy_storage_integration.json
+
+  permissions_boundary = var.iam_permissions_boundary
+}
+
+resource "aws_iam_role_policy_attachment" "role_policy_attachment" {
+  role       = join("", aws_iam_role.snowflakedb_load_role.*.name)
+  policy_arn = join("", aws_iam_policy.snowflakedb_load_policy.*.arn)
+}
---
+++ sf_users.tf
@@ -0,0 +1,99 @@
+resource "snowflake_role" "loader" {
+  name    = "${upper(var.name)}_LOADER_ROLE"
+}
+
+resource "snowflake_warehouse_grant" "loader" {
+  for_each          = toset(["MODIFY", "MONITOR", "USAGE", "OPERATE"])
+  warehouse_name    = snowflake_warehouse.loader.name
+  privilege         = each.key
+  roles             = [snowflake_role.loader.name]
+  with_grant_option = false
+}
+
+resource "snowflake_database_grant" "loader" {
+  database_name = snowflake_database.loader.name
+  privilege = "USAGE"
+  roles     = [snowflake_role.loader.name]
+  with_grant_option = false
+}
+
+resource "snowflake_file_format_grant" "loader" {
+  database_name     = snowflake_database.loader.name
+  schema_name       = snowflake_schema.atomic.name
+  file_format_name  = snowflake_file_format.enriched.name
+  privilege = "USAGE"
+  roles = [snowflake_role.loader.name]
+  with_grant_option = false
+}
+
+resource "snowflake_integration_grant" "loader" {
+  integration_name = snowflake_storage_integration.integration.name
+  privilege = "USAGE"
+  roles     = [snowflake_role.loader.name]
+  with_grant_option = false
+}
+
+resource "snowflake_stage_grant" "loader" {
+  database_name = snowflake_database.loader.name
+  schema_name   = snowflake_schema.atomic.name
+  stage_name    = snowflake_stage.loader.name
+  privilege = "USAGE"
+  roles  = [snowflake_role.loader.name]
+  with_grant_option = false
+}
+
+resource "snowflake_schema_grant" "loader" {
+  for_each = toset([
+      "CREATE EXTERNAL TABLE",
+      "CREATE FILE FORMAT",
+      "CREATE FUNCTION",
+      "CREATE MASKING POLICY",
+      "CREATE PIPE",
+      "CREATE PROCEDURE",
+      "CREATE SEQUENCE",
+      "CREATE STAGE",
+      "CREATE STREAM",
+      "CREATE TABLE",
+      "CREATE TASK",
+      "CREATE TEMPORARY TABLE",
+      "CREATE VIEW",
+      "MODIFY",
+      "MONITOR",
+      "USAGE"
+  ])
+  database_name = snowflake_database.loader.name
+  schema_name   = snowflake_schema.atomic.name
+  privilege = each.key
+  roles     = [snowflake_role.loader.name]
+  with_grant_option = false
+}
+
+resource "snowflake_table_grant" "loader" {
+  for_each = toset([
+    "DELETE",
+    "INSERT",
+    "OWNERSHIP",
+    "REFERENCES",
+    "SELECT",
+    "TRUNCATE",
+    "UPDATE"
+  ])
+  database_name = snowflake_database.loader.name
+  schema_name   = snowflake_schema.atomic.name
+  table_name    = snowflake_table.events.name
+  privilege = each.key
+  roles     = [snowflake_role.loader.name]
+  with_grant_option = false
+}
+
+resource "snowflake_user" "loader" {
+  name         = "${upper(var.name)}_LOADER_USER"
+  password     = var.sf_loader_password
+  default_role = snowflake_role.loader.name
+  must_change_password = false
+}
+
+resource "snowflake_role_grants" "loader" {
+  role_name = snowflake_role.loader.name
+  users = [snowflake_user.loader.name]
+}

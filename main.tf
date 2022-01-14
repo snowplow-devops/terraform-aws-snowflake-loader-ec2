34f674903f8ea3b47cb035f3f5935c09bed876a6
spenes <enes@snowplowanalytics.com>
2022-01-14 15:23:58 +0300

    Loader specific parts

--- main.tf
+++ main.tf
@@ -1,4 +1,23 @@
 locals {
+  module_name    = "snowflake-loader"
+  module_version = "0.1.0"
+
+  app_name    = "snowplow-snowflake-loader"
+  app_version = "2.0.0"
+
+  local_tags = {
+    Name           = var.name
+    app_name       = local.app_name
+    app_version    = local.app_version
+    module_name    = local.module_name
+    module_version = local.module_version
+  }
+
+  tags = merge(
+    var.tags,
+    local.local_tags
+  )
+
   account_id  = data.aws_caller_identity.current.account_id
   snowflake_load_role_name = "${var.name}-snowflakedb-load"
   snowflake_load_role_arn = "arn:aws:iam::${local.account_id}:role/${local.snowflake_load_role_name}"
--- variables.tf
+++ variables.tf
@@ -78,3 +78,157 @@ variable "iam_permissions_boundary" {
   default = ""
   type  = string
 }
+
+variable "vpc_id" {
+  description = "The VPC to deploy Loader within"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "The list of subnets to deploy Loader across"
+  type        = list(string)
+}
+
+variable "instance_type" {
+  description = "The instance type to use"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "associate_public_ip_address" {
+  description = "Whether to assign a public ip address to this instance"
+  type        = bool
+  default     = true
+}
+
+variable "ssh_key_name" {
+  description = "The name of the SSH key-pair to attach to all EC2 nodes deployed"
+  type        = string
+}
+
+variable "ssh_ip_allowlist" {
+  description = "The list of CIDR ranges to allow SSH traffic from"
+  type        = list(any)
+  default     = ["0.0.0.0/0"]
+}
+
+variable "amazon_linux_2_ami_id" {
+  description = "The AMI ID to use which must be based of of Amazon Linux 2; by default the latest community version is used"
+  default     = ""
+  type        = string
+}
+
+variable "tags" {
+  description = "The tags to append to this resource"
+  default     = {}
+  type        = map(string)
+}
+
+variable "cloudwatch_logs_enabled" {
+  description = "Whether application logs should be reported to CloudWatch"
+  default     = true
+  type        = bool
+}
+
+variable "cloudwatch_logs_retention_days" {
+  description = "The length of time in days to retain logs for"
+  default     = 7
+  type        = number
+}
+
+# --- Configuration options
+
+variable "sqs_queue_name" {
+  description = "SQS queue name"
+  type        = string
+}
+
+variable "db_host" {
+  description = "Redshift database host"
+  type        = string
+}
+
+variable "db_name" {
+  description = "Redshift database name"
+  type        = string
+}
+
+variable "db_port" {
+  description = "Redshift database port"
+  type        = string
+}
+
+variable "db_role_arn" {
+  description = "AWS Role ARN allowing Redshift to load data from S3"
+  type        = string
+}
+
+variable "db_schema" {
+  description = "DB schema name"
+  type        = string
+}
+
+variable "db_username" {
+  description = "DB user with permissions to load data"
+  type        = string
+}
+
+variable "db_password" {
+  description = "DB password"
+  type        = string
+}
+
+# --- Iglu Resolver
+
+variable "default_iglu_resolvers" {
+  description = "The default Iglu Resolvers that will be used by Stream Shredder"
+  default = [
+    {
+      name            = "Iglu Central"
+      priority        = 10
+      uri             = "http://iglucentral.com"
+      api_key         = ""
+      vendor_prefixes = []
+    },
+    {
+      name            = "Iglu Central - Mirror 01"
+      priority        = 20
+      uri             = "http://mirror01.iglucentral.com"
+      api_key         = ""
+      vendor_prefixes = []
+    }
+  ]
+  type = list(object({
+    name            = string
+    priority        = number
+    uri             = string
+    api_key         = string
+    vendor_prefixes = list(string)
+  }))
+}
+
+variable "custom_iglu_resolvers" {
+  description = "The custom Iglu Resolvers that will be used by Stream Shredder"
+  default     = []
+  type = list(object({
+    name            = string
+    priority        = number
+    uri             = string
+    api_key         = string
+    vendor_prefixes = list(string)
+  }))
+}
+
+# --- Telemetry
+
+variable "telemetry_enabled" {
+  description = "Whether or not to send telemetry information back to Snowplow Analytics Ltd"
+  type        = bool
+  default     = true
+}
+
+variable "user_provided_id" {
+  description = "An optional unique identifier to identify the telemetry events emitted by this stack"
+  type        = string
+  default     = ""
+}

e0a2149dc3f43c8cae0167c792625b2c2f5a941c
jbeemster <joshua.a.beemster@gmail.com>
2024-04-03 17:35:35 +0200

    Prepared for release

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 locals {
   module_name    = "snowflake-loader-ec2"
-  module_version = "0.3.0"
+  module_version = "0.3.1"
 
   app_name    = "rdb-loader-snowflake"
   app_version = var.app_version
